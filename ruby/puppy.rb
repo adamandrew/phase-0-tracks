4c058ddb46d4f55fd60f654c74aa6e6c7d64b960
Andrew Lee <adam7924adam@gmail.com>
2016-04-05 21:59:39 -0700

    add characteristics of puppy

---
+++ ruby/puppy.rb
@@ -0,0 +1,19 @@
+=begin
+SPECIES ----------------
+Puppy
+
+CHARACTERISTICS --------
+Friendly: yes
+Eye count: 2
+Leg count: 4
+Name: varies
+Color: varies
+
+BEHAVIOR ---------------
+Bark
+Sleep
+Run around
+Lick
+Eat
+Poo and pee
+=end
