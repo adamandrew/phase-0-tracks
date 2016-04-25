f7711ac5080da5f5ddfe379c5134d0815574e0c8
Andrew Lee <adam7924adam@gmail.com>
2016-04-25 04:52:46 -0700

    add methods to get student and subject id and method to display grade of a student

--- databases/grades/grades.rb
+++ databases/grades/grades.rb
@@ -4,7 +4,6 @@ require 'sqlite3'
 
 # create database
 db = SQLite3::Database.new("grades.db")
-db.results_as_hash = true
 
 create_students_table_cmd = <<-SQL
 	CREATE TABLE IF NOT EXISTS students(
@@ -47,8 +46,59 @@ def add_subject(database, name)
 	database.execute("INSERT INTO subjects (name) VALUES (?)", [name])
 end
 
-add_subject(db, "Math")
-add_subject(db, "English")
-add_subject(db, "Physics")
-add_subject(db, "Art")
-add_subject(db, "Chemistry")
+# add_subject(db, "Math")
+# add_subject(db, "English")
+# add_subject(db, "Physics")
+# add_subject(db, "Art")
+# add_subject(db, "Chemistry")
+
+# method to add a grade
+def add_grade(database, student_id, subject_id, grade)
+	database.execute("INSERT INTO grades (grade, student_id, subject_id) VALUES (?, ?, ?)", [grade, student_id, subject_id])
+end
+
+# add_grade(db, 1, 1, 80)
+
+# method to update a grade
+def update_grade(database, student_id, subject_id, grade)
+	database.execute("UPDATE grades SET grade=? WHERE student_id=? AND subject_id=?",[grade, student_id, subject_id])
+end
+
+# update_grade(db, 1, 1, 75)
+
+# method to get student id
+def get_student_id(database, name)
+	student = database.execute("SELECT * FROM students WHERE name=?",[name])
+	student_id = student[0][0]
+end
+
+# p get_student_id(db, "Bob")
+
+# method to get subject id
+def get_subject_id(database, name)
+	subject = database.execute("SELECT * FROM subjects WHERE name=?",[name])
+	subject_id = subject[0][0]
+end
+
+# p get_subject_id(db, "Math")
+
+# method to display all grades
+def display_grades(database, name)
+	student_id = get_student_id(database, name)
+	display_cmd= <<-SQL
+		SELECT students.name, subjects.name, grades.grade
+		FROM grades
+			JOIN students
+				ON grades.student_id=students.id
+			JOIN subjects
+				ON grades.subject_id=subjects.id
+		WHERE student_id=?
+	SQL
+	display = database.execute(display_cmd,[student_id])
+	display.each do |grade|
+		puts "#{grade[0]} scored a #{grade[2]} on #{grade[1]}."
+	end
+end
+
+# display_grades(db, "Bob")
+
